4ae3cb42e01848f13e7adedc3267c2d38134c570
MrYArg1cH <88245710+MrYArg1cH@users.noreply.github.com>
2024-03-09 20:29:15 +0700

    Add files via upload

---
+++ app.vue
@@ -0,0 +1,26 @@
+<script setup>
+useHead({
+  bodyAttrs: {
+    class: 'body'
+  }
+})
+</script>
+
+<template>
+    <Header>
+    </Header>
+
+    <main class="min-h-[calc(100vh-32.5rem)] xl:min-h-[calc(100vh-21rem)]">
+            <NuxtPage/>
+    </main>
+
+    <Footer>
+    </Footer>
+</template>
+
+<style>
+@font-face {
+  font-family: 'Comfortaa';
+  src: url('./assets/fonts/Comfortaa/Comfortaa-VariableFont_wght.ttf');
+}
+</style>
